7c774bed6895d2c2e614ebb3aaca66df7f2e2bea
Gummudu Bhargava Raju <bhargavgummudu@gmail.com>
2025-05-28 12:20:21 +0530

    Bhargav2 work this fils

---
+++ src/test/java/com/Normal/Normal/NormalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Normal.Normal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
